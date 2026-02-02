ea520a1a4ffbf0945000079a1926c783fa36e418
LewieJ08 <lewiej08@gmail.com>
2026-02-02 00:46:32 +0000

    Add initial user endpoints with fake data

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,13 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+import { userRouter } from './routes/users';
 
 const app = new Hono()
 
+// Middleware
 app.use(logger());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// Routes
+app.route('/api/users', userRouter)
 
 export default app
