7048ee79746f01c49c4ec05dd001a2c39e49d026
LewieJ08 <lewiej08@gmail.com>
2026-02-03 09:36:56 +0000

    Add cors middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,15 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+import { cors } from 'hono/cors';
 import { userRouter } from './routes/users';
 import { tasksRouter } from './routes/tasks';
 
 const app = new Hono()
 
 // Middleware
+app.use(cors({
+    origin: 'http://localhost:5173/'
+}));
 app.use(logger());
 
 // Routes
